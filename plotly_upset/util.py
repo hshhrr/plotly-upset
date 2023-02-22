c5267f0ce86bec434505d55d80216ddb2b80d4e9
Hasan Shahrier <66935697+hasan-shahrier@users.noreply.github.com>
2023-02-22 15:49:31 +0600

    Bug fixes

--- plotly_upset/util.py
+++ plotly_upset/util.py
@@ -86,70 +86,40 @@ def get_nodes_and_edges(n_sets: int):
     return (xt, yt), (xf, yf), edges
 
 
-def get_nonzero_nodes_and_edges(t: tuple, f: tuple, edges: list, n_sets: int, zero_indices: list):
-    xt, _ = t
-    xf, _ = f
-
-    nonzero_indices = np.setdiff1d(np.arange(0, 2 ** n_sets), zero_indices)
+def get_nonzero_nodes_and_edges(t: tuple, f: tuple, edges: list, nonzero_indices: list):
+    xt, yt = t
+    xf, yf = f
 
     # Non-Zero True Nodes
-    _xt = list()
-    for x in xt:
-        temp_x = list()
-        for v in x:
-            if v in zero_indices:
-                temp_x.append(-1)
-            else:
-                p = np.where(nonzero_indices == v)
+    _xt, _yt = list(), list()
+    for x, y in zip(xt, yt):
+        temp_x, temp_y = list(), list()
+        for _x, _y in zip(x, y):
+            if _x in nonzero_indices:
+                p = np.where(nonzero_indices == _x)
                 p = np.squeeze(p)[()]
                 temp_x.append(p)
-        _xt.append(temp_x)
-
-        
-    __xt = list()
-    __yt = list()
-
-    for x in _xt:
-        temp_x, temp_y = list(), list()
-        for i, v in enumerate(x):
-            if v != -1:
-                temp_x.append(v)
-                temp_y.append(i)
-
-        if len(temp_x) != 0:
-            __xt.append(temp_x)
-            __yt.append(temp_y)
+                temp_y.append(_y)
+        if len(temp_x) != 0 and len(temp_y) != 0:
+            _xt.append(temp_x)
+            _yt.append(temp_y)
 
     # Non-Zero False Nodes
-    _xf = list()
-    for x in xf:
-        temp_x = list()
-        for v in x:
-            if v in zero_indices:
-                temp_x.append(-1)
-            else:
-                p = np.where(nonzero_indices == v)
+    _xf, _yf = list(), list()
+    for x, y in zip(xf, yf):
+        temp_x, temp_y = list(), list()
+        for _x, _y in zip(x, y):
+            if _x in nonzero_indices:
+                p = np.where(nonzero_indices == _x)
                 p = np.squeeze(p)[()]
                 temp_x.append(p)
-        _xf.append(temp_x)
+                temp_y.append(_y)
+        if len(temp_x) != 0 and len(temp_y) != 0:
+            _xf.append(temp_x)
+            _yf.append(temp_y)
 
-        
-    __xf = list()
-    __yf = list()
-
-    for x in _xf:
-        temp_x, temp_y = list(), list()
-        for i, v in enumerate(x):
-            if v != -1:
-                temp_x.append(v)
-                temp_y.append(i)
-
-        if len(temp_x) != 0:
-            __xf.append(temp_x)
-            __yf.append(temp_y)
-
-    #  Non-Zero Edges
-    __edges = list()
+    # Non-Zero Edges
+    _edges = list()
     for e in edges:
         if e[0][0] in nonzero_indices:
             p = np.where(nonzero_indices == e[0][0])
@@ -158,9 +128,9 @@ def get_nonzero_nodes_and_edges(t: tuple, f: tuple, edges: list, n_sets: int, ze
             temp_e[0] = [p] * 2
             temp_e = np.array(temp_e).T
             temp_e = temp_e.tolist()
-            __edges.append(temp_e)
+            _edges.append(temp_e)
 
-    return (__xt, __yt), (__xf, __yf), __edges
+    return (_xt, _yt), (_xf, _yf), _edges
 
 
 def get_sorted_nodes_and_edges(t: tuple, f: tuple, edges: list, sorted_sequence: list):
@@ -168,15 +138,15 @@ def get_sorted_nodes_and_edges(t: tuple, f: tuple, edges: list, sorted_sequence:
     xf, yf = f
 
     _xt = np.concatenate(xt, axis=None)
-    __xt = np.array([np.squeeze(np.where(sorted_sequence == x))[()] for x in _xt])
-    __xt = __xt.reshape(xt.shape)
+    _xt = np.array([np.squeeze(np.where(sorted_sequence == x))[()] for x in _xt])
+    _xt = _xt.reshape(xt.shape)
 
     _xf = np.concatenate(xf, axis=None)
-    __xf = np.array([np.squeeze(np.where(sorted_sequence == x))[()] for x in _xf])
-    __xf = __xf.reshape(xf.shape)
+    _xf = np.array([np.squeeze(np.where(sorted_sequence == x))[()] for x in _xf])
+    _xf = _xf.reshape(xf.shape)
 
     #  Sorted Edges
-    __edges = list()
+    _edges = list()
     for e in edges:
         if e[0][0] in sorted_sequence:
             p = np.where(sorted_sequence == e[0][0])
@@ -185,6 +155,6 @@ def get_sorted_nodes_and_edges(t: tuple, f: tuple, edges: list, sorted_sequence:
             temp_e[0] = [p] * 2
             temp_e = np.array(temp_e).T
             temp_e = temp_e.tolist()
-            __edges.append(temp_e)
+            _edges.append(temp_e)
 
-    return (__xt, yt), (__xf, yf), __edges
+    return (_xt, yt), (_xf, yf), _edges
